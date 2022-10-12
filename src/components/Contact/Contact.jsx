f949b2c476586738071fb4c0a735ac630204898c
YasminaMvl <pro.yasminam@gmail.com>
2022-10-12 15:24:31 +0200

    feat: Form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,23 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+    <div className="container contact__container">
+    <div>
+        <form action="">
+          <input type="text" name='name' placeholder='Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="10" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'> Send Message</button>
+
+        </form>
+      </div>
+    </div>
+      
+      
+    </section>
   )
 }
 
